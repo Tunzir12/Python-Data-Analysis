a52c0784d7b36917e39a913dd72a6de08af3bf9f
Maliha Tunzira <33031428+Tunzir12@users.noreply.github.com>
2024-10-09 22:19:02 +0200

    task 1 done

--- src/main.py
+++ src/main.py
@@ -1,5 +1,21 @@
 import csv
 
-emisssion_path = "../data/Emissions.csv"
-f = open(emisssion_path,'r')
-print(f.read())
+filename = "../data/Emissions.csv"
+
+def csvToDictionary(filename): 
+    dataDictionary = {}
+    with open(filename,'r') as file:
+        csvRead = csv.reader(file)
+
+        for row in csvRead:
+            country = row[0]
+            values = row[1:]
+            dataDictionary[country] = values
+    
+    return dataDictionary
+
+
+data = csvToDictionary(filename)
+
+for key, value in data.items():
+    print(f'{key}:{value}')
