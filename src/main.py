5e5f1d7dd66bf90ccdc6eafd6fd2026bd9ee174a
Maliha Tunzira <33031428+Tunzir12@users.noreply.github.com>
2024-10-09 23:22:06 +0200

    emission analysis pending

--- src/main.py
+++ src/main.py
@@ -7,15 +7,34 @@ def csvToDictionary(filename):
     with open(filename,'r') as file:
         csvRead = csv.reader(file)
 
+        headers = next(csvRead)
+
         for row in csvRead:
             country = row[0]
             values = row[1:]
             dataDictionary[country] = values
     
-    return dataDictionary
+    return dataDictionary, headers
+
+def emissionYear(headers,year):
+    if year in headers:
+        year_index = headers.index(year) - 1
+    emissions_list = {}
+    for country, values in data.items():
+        emissions_list[country] = values[year_index]
+
+    #future works start here
+    
+    return minEmission, maxEmission, avgEmission
+
+
 
 
 data = csvToDictionary(filename)
 
-for key, value in data.items():
-    print(f'{key}:{value}')
+print("All data from {filename} has been read into a dictionary")
+
+yearInput = input("Select a year to find statistics (1997-2010):")
+print("In {yearInput}, countries with minimum and maximum CO2 emmission levels were [{minEmission}] and [{maxEmission}] respectively.\n Average CO2 emissions in {yearInput} were {avgEmission}")
+
+
